f391ceddd4a77b3e9904aeaca76bcabd43f5364a
austinslair <100247884+austinslair@users.noreply.github.com>
2026-09-17 17:22:49 -0400

    Create sal.h with input/output annotation macros
    This header file defines several macros for input/output annotations, which can be used for better code documentation and static analysis.

---
+++ android/app/src/main/cpp/sal.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#ifndef _In_
+#define _In_
+#endif
+#ifndef _Out_
+#define _Out_
+#endif
+#ifndef _Inout_
+#define _Inout_
+#endif
+#ifndef _In_z_
+#define _In_z_
+#endif
+#ifndef _Out_z_
+#define _Out_z_
+#endif
+#ifndef _In_opt_
+#define _In_opt_
+#endif
+#ifndef _Out_opt_
+#define _Out_opt_
+#endif
+#ifndef _Inout_opt_
+#define _Inout_opt_
+#endif
+#ifndef _Field_size_
+#define _Field_size_(x)
+#endif
+#ifndef _Field_size_opt_
+#define _Field_size_opt_(x)
+#endif
+#ifndef _Field_size_bytes_
+#define _Field_size_bytes_(x)
+#endif
+#ifndef _Printf_format_string_
+#define _Printf_format_string_
+#endif
+#ifndef _Scanf_format_string_
+#define _Scanf_format_string_
+#endif
+#ifndef _Ret_maybenull_
+#define _Ret_maybenull_
+#endif
+#ifndef _Ret_notnull_
+#define _Ret_notnull_
+#endif
+#ifndef _Check_return_
+#define _Check_return_
+#endif
+#ifndef _Success_
+#define _Success_(x)
+#endif
