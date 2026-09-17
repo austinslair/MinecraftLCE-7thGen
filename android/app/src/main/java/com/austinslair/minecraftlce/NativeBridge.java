9ddb6eade94713027a94d217801e972260e62c93
austinslair <100247884+austinslair@users.noreply.github.com>
2026-09-17 14:00:51 -0400

    Expand native renderer lifecycle bridge

--- android/app/src/main/java/com/austinslair/minecraftlce/NativeBridge.java
+++ android/app/src/main/java/com/austinslair/minecraftlce/NativeBridge.java
@@ -1,5 +1,10 @@
 package com.austinslair.minecraftlce;
 
 public final class NativeBridge {
+    private NativeBridge() {}
+
     public static native void touch(int action, int pointerId, float x, float y);
+    public static native void surfaceCreated();
+    public static native void surfaceChanged(int width, int height);
+    public static native void renderFrame();
 }
