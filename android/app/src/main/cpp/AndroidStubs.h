3ce51c0dfeb46f394066539fb2cee4f2bc4d4635
austinslair <100247884+austinslair@users.noreply.github.com>
2026-09-17 20:34:23 -0400

    Add AndroidStubs.h for Windows and Xbox compatibility

---
+++ android/app/src/main/cpp/AndroidStubs.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "AndroidTypes.h"
+#include <pthread.h>
+#include <unistd.h>
+
+#define _ASSERT(x) ((void)0)
+
+struct AndroidCriticalSection {
+    pthread_mutex_t mutex;
+};
+
+typedef struct AndroidCriticalSection CRITICAL_SECTION;
+typedef CRITICAL_SECTION* PCRITICAL_SECTION;
+typedef CRITICAL_SECTION* LPCRITICAL_SECTION;
+
+void InitializeCriticalSection(CRITICAL_SECTION* cs);
+void DeleteCriticalSection(CRITICAL_SECTION* cs);
+void EnterCriticalSection(CRITICAL_SECTION* cs);
+void LeaveCriticalSection(CRITICAL_SECTION* cs);
+BOOL TryEnterCriticalSection(CRITICAL_SECTION* cs);
+
+DWORD TlsAlloc(void);
+BOOL TlsFree(DWORD dwTlsIndex);
+LPVOID TlsGetValue(DWORD dwTlsIndex);
+BOOL TlsSetValue(DWORD dwTlsIndex, LPVOID lpTlsValue);
+
+void Sleep(DWORD dwMilliseconds);
+BOOL CloseHandle(HANDLE hObject);
+DWORD WaitForSingleObject(HANDLE hHandle, DWORD dwMilliseconds);
