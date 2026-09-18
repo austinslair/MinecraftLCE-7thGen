6045056e108b0c3b33a921a8092c4525b7f2e01f
austinslair <100247884+austinslair@users.noreply.github.com>
2026-09-17 21:40:16 -0400

    Add initAssets and setScreen to NativeBridge.java

--- android/app/src/main/java/com/austinslair/minecraftlce/NativeBridge.java
+++ android/app/src/main/java/com/austinslair/minecraftlce/NativeBridge.java
@@ -1,5 +1,7 @@
 package com.austinslair.minecraftlce;
 
+import android.content.res.AssetManager;
+
 public final class NativeBridge {
     static {
         System.loadLibrary("minecraft_android");
@@ -7,8 +9,10 @@ public final class NativeBridge {
 
     private NativeBridge() {}
 
+    public static native void initAssets(AssetManager assetManager);
     public static native void touch(int action, int pointerId, float x, float y);
     public static native void surfaceCreated();
     public static native void surfaceChanged(int width, int height);
     public static native void renderFrame();
+    public static native void setScreen(int screenId); // 0 = Main Menu, 1 = Play/World
 }
