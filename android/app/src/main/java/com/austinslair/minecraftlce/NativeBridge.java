32112264219f62fe1c28a03c72cb3bae7ea49a0f
austinslair <100247884+austinslair@users.noreply.github.com>
2026-09-18 11:46:12 -0400

    Route Android startup through native platform bridge

--- android/app/src/main/java/com/austinslair/minecraftlce/NativeBridge.java
+++ android/app/src/main/java/com/austinslair/minecraftlce/NativeBridge.java
@@ -9,7 +9,7 @@ public final class NativeBridge {
 
     private NativeBridge() {}
 
-    public static native void initAssets(AssetManager assetManager);
+    public static native void initPlatform(AssetManager assetManager);
     public static native void touch(int action, int pointerId, float x, float y);
     public static native void surfaceCreated();
     public static native void surfaceChanged(int width, int height);
